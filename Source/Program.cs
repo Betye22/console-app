bc345450818c7492725c97ec89b0ea87f8a5d17e
Asuncion <0619764977@uma.es>
2025-10-21 07:54:11 +0200

    console app project

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace ConsoleApp;
+class Program
+{
+static void Main(string[] args)
+{
 Console.WriteLine("Hello, World!");
+}
+}
