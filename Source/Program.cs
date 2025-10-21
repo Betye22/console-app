43dd721194146000b8b51712fbc74ce3925364b3
Asuncion <0619764977@uma.es>
2025-10-21 08:04:15 +0200

    mod-2: Hello new world + Hello 2

--- Source/Program.cs
+++ Source/Program.cs
@@ -3,6 +3,8 @@ class Program
 {
 static void Main(string[] args)
 {
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, new World!");
+        Console.WriteLine("Hello 2");
+
 }
 }
